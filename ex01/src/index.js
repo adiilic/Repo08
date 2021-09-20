bd79a6383e2c6d52a70602b868c7ae7708e3cb88
adiilic <adiilic12@gmail.com>
2021-09-20 21:43:48 +0200

    Day 08

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function reusableFunction() {
+
+    console.log("Hi Padawan!");
+
+};
+reusableFunction();
+
+// Only change code above this line
+module.exports = reusableFunction;
