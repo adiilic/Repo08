bb12aa9e135380b54e70f081039f3dae17d901b5
adiilic <adiilic12@gmail.com>
2021-09-20 23:54:49 +0200

    index

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -3,8 +3,8 @@ function randomRangeNumber(minNumber, maxNumber) {
     return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
 };
-var random = randomRangeNumber(1,9);
-console.log(random);
+randomRangeNumber();
+console.log(randomRangeNumber(1, 9));
 
 // Only change code above this line
 module.exports = randomRangeNumber;
